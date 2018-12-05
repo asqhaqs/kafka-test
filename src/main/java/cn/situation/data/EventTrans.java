9e39e6e1aba8475df83019d8e3ce735a62f9bd95
pengbowen <pengbowen@b.360.cn>
2018-12-05 11:33:04 +0800

    添加字段转换

--- src/main/java/cn/situation/data/EventTrans.java
+++ src/main/java/cn/situation/data/EventTrans.java
@@ -15,37 +15,40 @@ import cn.situation.util.Geoip;
 import cn.situation.util.JsonUtil;
 import cn.situation.util.LogUtil;
 
-/**
- * 告警数据映射转换
- */
+
 public class EventTrans {
-
 	private static final Logger LOG = LogUtil.getInstance(EventTrans.class);
+	
 	private static final String redisAlertKey = SystemConstant.REDIS_KEY_PREFIX + ":" + SystemConstant.REDIS_ALERT_KEY;
-
-	public static Object do_trans(List<String> e_list) throws Exception {
+	/**
+	 * situation—ids转换
+	 * @param e_list
+	 * @throws Exception
+	 */
+	public static void do_trans(List<String> e_list) throws Exception {
 		for (String row : e_list) {
-			String[] fileds = row.split("//|");
+			String s_tmp = row.replace("|", "%");
+			String[] fileds = s_tmp.split("%");
 			do_map(fileds);
 		}
-		return null;
 	}
-	
-	public static Object do_map(String[] fileds) throws Exception {
-//		LOG.info(String.format("[%s]: isAlert<%s>, message<%s>", "mapAndEnrichOperation"));
-		
+	/**
+	 * 数据映射入库（redis）
+	 * @param fileds
+	 * @throws Exception
+	 */
+	private static void do_map(String[] fileds) throws Exception {
+		LOG.info(String.format("[]:  message<%s>", "mapAndEnrichOperation"), "web-ids data start");
+
 		Map<String, Object> syslogMap = new HashMap<>();
 
 		// 导入map中
-		// fillToMap(syslogMap, alertFields, fieldList);
+		syslogMap=fillToMap(syslogMap, fileds);
+		System.out.println(syslogMap.get("timestamp"));
 		
-		// // 加入 360 字段，全部置空
-		// for (String qhField : qhAlertFields) {
-		// syslogMap.put(qhField, null);
-		// }
-		 // sip 和 dip 进行 ip 富化
-		 enrichmentIp(syslogMap, true);
-
+		// sip 和 dip 进行 ip 富化
+		enrichmentIp(syslogMap, true);
+		
 		// 添加公共头使得该条告警通过规则引擎
 		syslogMap.put("event_id", UUID.randomUUID().toString());
 		syslogMap.put("found_time",
@@ -58,15 +61,15 @@ public class EventTrans {
 		syslogMap.put("sip", syslogMap.get("sip").toString());
 		syslogMap.put("dip", syslogMap.get("dip").toString());
 		syslogMap.put("organization_id", 0);
+		
+		//入redis库
 		String resultJson = JsonUtil.mapToJson(syslogMap);
+		System.out.println(resultJson);
 		DicUtil.rpush(redisAlertKey, resultJson);
 		LOG.info(String.format("[%s]: dicName<%s>, value<%s>", "mapAndEnrichOperation", redisAlertKey, resultJson));
-		return null;
 	}
-
 	/**
-	 * 富化ip(sip、dip; Webids：victim、attacker)
-	 * 
+	 * 富化ip
 	 * @param syslogMap
 	 * @param isAleart
 	 * @throws Exception
@@ -74,16 +77,9 @@ public class EventTrans {
 	private static void enrichmentIp(Map<String, Object> syslogMap, Boolean isAleart) throws Exception {
 		String sipStr;
 		String dipStr;
-		// if (!isAleart) {
-		// // （1）sip、dip
+
 		sipStr = (null == syslogMap.get("sip")) ? null : (String) syslogMap.get("sip");
 		dipStr = (null == syslogMap.get("dip")) ? null : (String) syslogMap.get("dip");
-		// } else {
-		// sipStr = (null == syslogMap.get("sip")) ? null :
-		// (String)syslogMap.get("src_ip");
-		// dipStr = (null == syslogMap.get("dip")) ? null :
-		// (String)syslogMap.get("dst_ip");
-		// }
 
 		Geoip.Result sipResult = Geoip.getInstance().query(sipStr);
 		Geoip.Result dipResult = Geoip.getInstance().query(dipStr);
@@ -97,15 +93,110 @@ public class EventTrans {
 		} else {
 			syslogMap.put("geo_sip", null);
 		}
-
 		if (dipMap != null) {
 			syslogMap.put("geo_dip", dipMap);
 		} else {
 			syslogMap.put("geo_dip", null);
 		}
-		// 此富化没有 webshell， webattack类型
 	}
+	/**
+	 * 厂商数据转换
+	 * @param map
+	 * @param fields
+	 * @return Map<String,Object
+	 * @throws NullPointerException
+	 * @throws ArrayIndexOutOfBoundsException
+	 */
+	private static Map<String, Object> fillToMap(Map<String, Object> map, String[] fields)
+			throws NullPointerException, ArrayIndexOutOfBoundsException {
+		Map<String, Object> map_tmp = new HashMap<String, Object>();
+		//厂商字段梳理
+		map_tmp.put("version", fields[0]);
+		map_tmp.put("ip_type", fields[1]);
+		map_tmp.put("msgtype", fields[2]);
+		map_tmp.put("magic_number", fields[3]);
+		map_tmp.put("timestamp", fields[4]);
+		map_tmp.put("pkt_size", fields[5]);
+		map_tmp.put("dev_ip", fields[6]);
+		map_tmp.put("vendor", fields[7]);
+		map_tmp.put("sip", fields[8]);
+		map_tmp.put("dip", fields[9]);
+		map_tmp.put("sport", fields[10]);
+		map_tmp.put("dport", fields[11]);
+		map_tmp.put("protocol_id", fields[12]);
+		map_tmp.put("app_version", fields[13]);
+		map_tmp.put("dev_id", fields[14]);
+		map_tmp.put("sid", fields[15]);
+		map_tmp.put("direct", fields[16]);
+		map_tmp.put("attack_ip", fields[17]);
+		map_tmp.put("attack_ipv6", fields[18]);
+		map_tmp.put("victim_ip", fields[19]);
+		map_tmp.put("victim_ipv6", fields[20]);
+		map_tmp.put("eventcode", fields[21]);
+		map_tmp.put("ruleid", fields[22]);
+		map_tmp.put("vid", fields[23]);
+		map_tmp.put("last_times", fields[24]);
+		map_tmp.put("event_id", fields[25]);
+		map_tmp.put("event_info", fields[26]);
+		map_tmp.put("proof", fields[27]);
 
+		// 转换
+		//厂商字段
+		map.put("protocol_id", fields[12]);
+		map.put("app_version", fields[13]);
+		map.put("dev_id", fields[14]);
+		map.put("sid", fields[15]);
+		map.put("direct", fields[16]);
+		map.put("attack_ip", fields[17]);
+		map.put("attack_ipv6", fields[18]);
+		map.put("victim_ip", fields[19]);
+		map.put("victim_ipv6", fields[20]);
+		map.put("eventcode", fields[21]);
+		map.put("ruleid", fields[22]);
+		map.put("vid", fields[23]);
+		map.put("last_times", fields[24]);
+		map.put("event_id", fields[25]);
+		map.put("event_info", fields[26]);
+		map.put("proof", fields[27]);
+		map.put("version", fields[0]);
+		map.put("ip_type", fields[1]);
+		map.put("msgtype", fields[2]);
+		map.put("magic_number", fields[3]);
+		map.put("timestamp", fields[4]);
+		map.put("pkt_size", fields[5]);
+		map.put("dev_ip", fields[6]);
+		map.put("vendor", fields[7]);
+		map.put("sip", fields[8]);
+		map.put("dip", fields[9]);
+		
+		//360字段
+		map.put("attack_type", "");
+		map.put("bugtraq", "");
+		map.put("cve", "");
+		map.put("data_type", "");
+		map.put("dcity", "");
+		map.put("dcounty", "");
+		map.put("description", "");
+		map.put("dlatitude", "");
+		map.put("dlongitude", "");
+		map.put("dprovince", "");
+		map.put("protocol", "");
+		map.put("rule_num", "");
+		map.put("solution", "");
+		map.put("threat_level", "");
+		map.put("user", "");
+		map.put("s_location", "");
+		map.put("d_location", "");
+		map.put("event_token", "");
+		map.put("back_up", "");
+		
+		//共有字段
+		map.put("dport", map_tmp.get("dport"));
+		map.put("sport", map_tmp.get("sport"));
+		map.put("sip", map_tmp.get("sip"));
+		map.put("dip", map_tmp.get("dip"));
+		return map;
+	}
 	public static void main(String[] args) throws Exception {
 		List<String> s_list = new ArrayList<String>();
 		s_list.add(
@@ -128,21 +219,4 @@ public class EventTrans {
 				"0x01|0x01|0x020B|0x01|1541560149|100|127.0.0.1|360|1.2.3.4|4.3.2.1|0|0| tcp|||12345678qwertyuiasdfghjkzxcvbnml123456|01|127.0.0.1||127.0.0.1||1||||5|ddos|{“http_keypayload”: “ab%%%cdef^^^111111”}\r\n");
 		do_trans(s_list);
 	}
-
-	// /**
-	// * 将日志中的数据导入至map中
-	// * @param map
-	// * @param fields
-	// * @param logValues
-	// * @throws NullPointerException
-	// * @throws ArrayIndexOutOfBoundsException
-	// */
-	// private void fillToMap(Map<String,Object> map, String[] fields, List<String>
-	// logValues)
-	// throws NullPointerException,ArrayIndexOutOfBoundsException{
-	// //对syslog 内容数组而不是字段数组进行遍历，防止因为数据过长截断而导致的数组越界问题
-	// for (int i = 0; i < logValues.size(); i++) {
-	// map.put(fields[i].trim(), logValues.get(i).trim());
-	// }
-	// }
 }
