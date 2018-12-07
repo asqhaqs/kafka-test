f143a70a280a56befe96faffe8d4d5e2972e52d3
quanli <13026148179@163.com>
2018-12-07 16:17:51 +0800

    资产解析修改

--- src/main/java/cn/situation/util/PgUtil.java
+++ src/main/java/cn/situation/util/PgUtil.java
@@ -2,10 +2,11 @@ package cn.situation.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import cn.situation.cons.SystemConstant;
 
@@ -14,10 +15,12 @@ import cn.situation.cons.SystemConstant;
  * @author quanli
  */
 public class PgUtil {
-	private final Logger logger = LogUtil.getInstance(PgUtil.class);
+private final Logger logger = LoggerFactory.getLogger(PgUtil.class);
 	
 	private static PgUtil instance = new PgUtil();
 	private Connection conn = null;
+	private PreparedStatement pre = null;
+	private ResultSet res = null;
 	
 	public static PgUtil getInstance() {
 		return instance;
@@ -38,31 +41,37 @@ public class PgUtil {
 			
 			Class.forName("org.postgresql.Driver");
 			conn = DriverManager.getConnection(url, userName, password);
+			conn.setAutoCommit(true);
 		} catch (Exception e) {
 			logger.error(String.format("postgresql连接信息获取失败, 错误信息[%s]", e.getMessage()));
 		}
 		return conn;
 	}
 	
+	public PreparedStatement getPreparedStatement(String sql) throws Exception {
+		return getConnection().prepareStatement(sql);
+	}
+	
 	/**
 	 * 销毁连接信息
 	 */
-	public void destory(Connection connection, Statement stat, ResultSet res) {
+	public void destory() {
 		try {
 			if(res != null) {
 				res.close();
 				res = null;
 			}
-			if(stat != null) {
-				stat.close();
-				stat = null;
+			if(pre != null) {
+				pre.close();
+				pre = null;
 			}
-			if(connection != null) {
-				connection.close();
-				connection = null;
+			if(conn != null) {
+				conn.close();
+				conn = null;
 			}
 		} catch (Exception e) {
 			logger.error(String.format("连接信息关闭失败, 错误信息为:[%s]", e.getMessage()));
 		}
 	}
+	
 }
